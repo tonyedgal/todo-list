d39c2512de3aec6f95a564dcd12619a8e10b367d
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-27 12:46:37 +0100

    feat: Added TodoList repo and set model relations
    - Added TodoList repository to src/repositories
    - Defined model relation between Todo and TodoList with hasMany and belongsTo relation types
    - Inclusion of related models in the Todo and TodoList models
    - Updated sample data in db.json with TodoList and added todoListId to individual todos

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,4 +1,5 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {TodoList, TodoListWithRelations} from './todo-list.model';
 
 @model()
 export class Todo extends Entity {
@@ -25,6 +26,8 @@ export class Todo extends Entity {
   })
   isComplete?: boolean;
 
+  @belongsTo(() => TodoList)
+  todoListId: number;
 
   constructor(data?: Partial<Todo>) {
     super(data);
@@ -33,6 +36,7 @@ export class Todo extends Entity {
 
 export interface TodoRelations {
   // describe navigational properties here
+  todoList?: TodoListWithRelations;
 }
 
 export type TodoWithRelations = Todo & TodoRelations;
